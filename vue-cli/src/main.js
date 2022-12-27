5bd91f2c9254711acac52d885eca0782b054f3c2
taykim7 <th_77@naver.com>
2022-12-27 22:58:10 +0900

    뷰프로젝트

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -3,6 +3,20 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// var App = {
+//   template: '<div>app</div>'
+// }
+
 new Vue({
   render: h => h(App),
+  // 이렇게 컴포넌트하는 것과 render 과 같음
+  // components: {
+  //   'app': App
+  }
 }).$mount('#app')
+
+// 동일함
+// new Vue({
+//   el: '#app',
+//   render: h => h(App),
+// })

9f477089c62d3f08a1618697951caa469b14611b
taykim7 <th_77@naver.com>
2023-01-08 21:53:02 +0900

    todo

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>
-    헤더입니다
-  </div>
+  <header>
+    <h1>투두리스트</h1>
+  </header>
 </template>
 
-<script>
-export default {
-
+<style scoped>
+h1 {
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
 }
-</script>
-
-<style>
-
 </style>

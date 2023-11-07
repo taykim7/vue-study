83d5edc578b61fda490784c5dfba989386c6c17e
taykim7 <th_77@naver.com>
2023-11-07 20:40:02 +0900

    프로젝트 구성 - 라우터링크 연결

---
+++ vue-advanced/vue-news/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <tool-bar></tool-bar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import ToolBar from './components/ToolBar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ToolBar,
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ vue-advanced/vue-news/src/components/ToolBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-link to="/news">NEWS</router-link>
+    <router-link to="/ask">ASK</router-link>
+    <router-link to="/jobs">JOBS</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

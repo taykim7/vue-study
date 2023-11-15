6fdc6fd23947c4a71439d427e14e73ea6f5ee61e
taykim7 <th_77@naver.com>
2023-11-15 20:26:04 +0900

    vue transition 적용

--- vue-advanced/vue-news/src/App.vue
+++ vue-advanced/vue-news/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <tool-bar></tool-bar>
-    <router-view></router-view>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -20,5 +22,12 @@ export default {
 body {
   padding: 0;
   margin: 0;
+}
+/* 라우터 트랜지션 */
+.page-enter-active, .page-leave-active {
+  transition: opacity .5s;
+}
+.page-enter, .page-leave-to {
+  opacity: 0;
 }
 </style>

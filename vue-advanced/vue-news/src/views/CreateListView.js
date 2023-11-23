14395e369d790da90f9e45cfdf84d9ffe82f5352
taykim7 <th_77@naver.com>
2023-11-23 21:31:55 +0900

    하이오너 컴포넌트 구분

--- vue-advanced/vue-news/src/views/CreateListView.js
+++ vue-advanced/vue-news/src/views/CreateListView.js
@@ -1,13 +1,17 @@
 import ListView from './ListView.vue'
 import bus from '../utils/bus';
 
+// 하이오너 컴포넌트
+// 비슷한 컴포넌트를 재활용함
 export default function createListView(name) {
   // export default로 잽다 function을 하나 꺼내는 것
   return {
     // 재사용할 인스턴스, 컴포넌트 옵션들이 들어갈 곳
-    name: name,
+    //name,
+    name: 'HOC COMPONENT ' + name,
     created() {
       // 패치를 하는 동안 로딩 띄우기
+      bus.$emit('start:spinner');
       this.$store.dispatch('FETCH_LIST', this.$route.name)
       .then(() => {
         bus.$emit('end:spinner');

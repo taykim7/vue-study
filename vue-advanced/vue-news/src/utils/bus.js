210da6e8ef3a795d55b64ab1827de7d3ad719011
taykim7 <th_77@naver.com>
2023-11-23 20:49:40 +0900

    오타수정

--- vue-advanced/vue-news/src/utils/bus.js
+++ vue-advanced/vue-news/src/utils/bus.js
@@ -4,11 +4,11 @@ import Vue from 'vue';
 // 빈 이벤트 객체를 통해 컴포넌트간 데이터 전달
 
 // (1) export 첫번째 방법
-export const bus = new Vue();
+//export const bus = new Vue();
 // 받는 쪽에서는 아래처럼 
 // import { bus } from './bus.js';
 
 // (2) export 두번째 방법
-// export default new Vue();
+export default new Vue();
 // 받는 쪽에서는 아래처럼
 // import bus from './bus.js';

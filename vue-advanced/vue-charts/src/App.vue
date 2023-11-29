f2e2d5b22af2e9bc58d5d1d0498562b6a5118000
taykim7 <th_77@naver.com>
2023-11-29 19:29:08 +0900

    Chart js 활용 프로젝트 생성
    - Chart 라이브러리 컴포넌트 분리

---
+++ vue-advanced/vue-charts/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Chart</h1>
+    <bar-chart />
+  </div>
+
+</template>
+
+<script>
+import BarChart from './components/BarChart.vue'
+
+export default {
+  components: {
+    BarChart,
+  }
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ vue-advanced/vue-charts/src/components/BarChart.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <canvas id="myChart" width="400" height="400"></canvas>
+  </div>
+</template>
+
+<script>
+import { Chart, registerables } from 'chart.js'
+Chart.register(...registerables);
+
+export default {
+  mounted() {
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+}
+</script>
+
+<style>
+
+</style>

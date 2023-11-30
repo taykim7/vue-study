433c3b31eb01c0e2b220c1d7fee1a9fb01c68a1b
taykim7 <th_77@naver.com>
2023-11-30 19:40:50 +0900

    디자인 패턴 Controlled
    - 결합력이 높은 컴포넌트
    - props로 전달하지 않고 v-model 활용
    - v-model 은 @input이벤트 + :value

---
+++ vue-advanced/design/controlled/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+  <!-- props로 전달하지 않고 v-model 활용 -->
+  <!-- <check-box :checked="checked"></check-box> -->
+  <check-box v-model="checked"></check-box>
+  checked는 {{ checked }}
+  </div>
+</template>
+
+<script>
+import CheckBox from './components/CheckBox.vue';
+
+export default {
+  components: {
+    CheckBox
+  },
+  data() {
+    return {
+      checked: true,
+    }
+  }
+}
+</script>
---
+++ vue-advanced/design/controlled/src/components/CheckBox.vue
@@ -0,0 +1,16 @@
+<template>
+  <input type="checkbox" :value="appValue" @click="toggleCheckBox">
+</template>
+
+<script>
+export default {
+  // v-model이 :value로 내려옴
+  props: ['appValue'],
+  methods: {
+    toggleCheckBox() {
+      // 하위에서 상위로 올리기
+      this.$emit('input', !this.value);
+    }
+  }
+}
+</script>

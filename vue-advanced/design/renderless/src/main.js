b1b4263f03c04ed77fc3a5647cf60a2baf834af9
taykim7 <th_77@naver.com>
2023-11-30 19:55:13 +0900

    render 함수 간단 사용

--- vue-advanced/design/renderless/src/main.js
+++ vue-advanced/design/renderless/src/main.js
@@ -5,18 +5,18 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  // 1
+  // 1. 기본 구조
   render: function(createElement) {
     return createElement(App);
   },
-  // 2
+  // 2. 하이퍼스크립트(h) 활용 - '버츄얼돔' 을 지칭함
   render: function(h) {
     return h(App);
   },
-  // 3
+  // 3. 화살표함수 활용
   render: (h) => {
     return h(App);
   },
-  // 4
+  // 4. 인자가 1개라서 축소
   render: h => h(App),
 }).$mount('#app')

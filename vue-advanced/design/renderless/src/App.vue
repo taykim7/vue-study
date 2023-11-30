ec2d6e14e2cffa76b10a2ec63d183bd32094ebbf
taykim7 <th_77@naver.com>
2023-11-30 20:04:34 +0900

    디자인 패턴 Renderless 추가
    - scoptedSlots로 데이터 가져오기

--- vue-advanced/design/renderless/src/App.vue
+++ vue-advanced/design/renderless/src/App.vue
@@ -2,7 +2,16 @@
   <div>
     <!-- open api -->
     <fetch-data url="https://jsonplaceholder.typicode.com/users/1">
-      <!-- ... -->
+      <!-- slot-scope -->
+      <div slot-scope="{ response, loading}">
+        <div v-if="!loading">
+          {{ response }}
+        </div>
+        <div v-else>
+         로딩중
+        </div>
+      </div>
+      
     </fetch-data>
   </div>
 </template>
@@ -12,7 +21,9 @@ import FetchData from './components/FetchData.vue'
 
 export default {
   components: {
+    // ↓ 여기 데이터를 가져옴
     FetchData
   },
+  // 대부분 그냥 여기서 created로 가져옴
 }
 </script>
--- vue-advanced/design/renderless/src/components/FetchData.vue
+++ vue-advanced/design/renderless/src/components/FetchData.vue
@@ -22,6 +22,7 @@ export default {
   },
   // render 함수
   render() {
+    // scopedSlots : 하위 컴포넌트의 데이터(response, loading)에 접근할 수 있음
     return this.$scopedSlots.default({
       response: this.response,
       loading: this.loading,

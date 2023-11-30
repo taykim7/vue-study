c0e97735ba2c6d4d7e721d8424f3c5e4722d7c39
taykim7 <th_77@naver.com>
2023-11-30 19:52:26 +0900

    디자인 패턴 Renderless
    - 데이터 처리 컴포넌트 (템플릿 표현식이 없음)
     - render

---
+++ vue-advanced/design/renderless/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- open api -->
+    <fetch-data url="https://jsonplaceholder.typicode.com/users/1">
+      <!-- ... -->
+    </fetch-data>
+  </div>
+</template>
+
+<script>
+import FetchData from './components/FetchData.vue'
+
+export default {
+  components: {
+    FetchData
+  },
+}
+</script>

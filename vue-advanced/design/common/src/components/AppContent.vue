a21a68e01cfc755df887f0d5436af535ce20a144
taykim7 <th_77@naver.com>
2023-11-30 19:09:43 +0900

    디자인 패턴 Common
    - 간단하고 기본적인 패턴

---
+++ vue-advanced/design/common/src/components/AppContent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in items">
+        {{ item }}
+      </li>
+    </ul>
+    <button @click="$emit('renew')">renew items</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      // 필수값 정의
+      required: true,
+    },
+  },
+}
+</script>

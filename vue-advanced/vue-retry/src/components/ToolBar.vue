6829217cce599e8ef56d0fdd3499f07944fc5391
taykim7 <th_77@naver.com>
2023-12-06 20:36:07 +0900

    계획

--- vue-advanced/vue-retry/src/components/ToolBar.vue
+++ vue-advanced/vue-retry/src/components/ToolBar.vue
@@ -2,7 +2,11 @@
   <div>
     <router-link to="/first">첫번째 방식</router-link> |
     <router-link to="/second">두번째 방식</router-link> |
-    <router-link to="/third">세번째 방식</router-link>
+    <router-link to="/">공통 컴포넌트 활용</router-link> |
+    <router-link to="/">slot 활용</router-link> |
+    <router-link to="/">eventbus 활용</router-link> |
+    <router-link to="/">HOC(하이오더컴포넌트) 활용</router-link> |
+    <router-link to="/">mixin 활용</router-link>
   </div>
 </template>
 

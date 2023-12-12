d94bfc9ff460ceb2ba8dcdd776aa38ef78b989c0
taykim7 <th_77@naver.com>
2023-12-12 23:57:47 +0900

    정리6 Hoc 활용 예제

--- vue-advanced/vue-retry/src/components/ToolBar.vue
+++ vue-advanced/vue-retry/src/components/ToolBar.vue
@@ -5,7 +5,8 @@
     <router-link to="/third">공통 컴포넌트 활용</router-link> |
     <router-link to="/slot">slot 활용</router-link> |
     <router-link to="/eventbus">eventbus 활용</router-link> |
-    <router-link to="/">HOC(하이오더컴포넌트) 활용</router-link> |
+    <router-link to="/usehoc1">HOC(하이오더컴포넌트) 활용1</router-link> |
+    <router-link to="/usehoc2">HOC(하이오더컴포넌트) 활용2</router-link> |
     <router-link to="/">mixin 활용</router-link>
   </div>
 </template>

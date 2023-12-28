610a2a0e975a2b582ef907c4deb719f7bee1fdae
taykim7 <th_77@naver.com>
2023-12-28 20:16:37 +0900

    submit event

--- vue-endgame/vue-til/src/components/SignupForm.vue
+++ vue-endgame/vue-til/src/components/SignupForm.vue
@@ -1,16 +1,16 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div>
       <label for="username">아디 : </label>
-      <input id="username" type="text">
+      <input id="username" type="text" v-model="username">
     </div>
     <div>
       <label for="password">비번 : </label>
-      <input id="password" type="text">
+      <input id="password" type="text" v-model="password">
     </div>
     <div>
       <label for="nickname">닉넴 : </label>
-      <input id="nickname" type="text">
+      <input id="nickname" type="text" v-model="nickname">
     </div>
     <button type="submit">가입</button>
   </form>
@@ -18,7 +18,18 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      username: '',
+      password: '',
+      nickname: '',
+    }
+  },
+  methods: {
+      submitForm() {
+        alert('폼제출')
+      }
+  }
 }
 </script>
 

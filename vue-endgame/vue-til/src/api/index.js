b5fce313564c69321db0f218e8fa257ae3b78c05
taykim7 <th_77@naver.com>
2023-12-30 22:40:02 +0900

    api 설정 공통화
    - axios.create 활용

--- vue-endgame/vue-til/src/api/index.js
+++ vue-endgame/vue-til/src/api/index.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
+const instance = axios.create({
+  // 공통 설정을 미리 넣을 수 있음
+  baseURL: 'http://localhost:3000',
+});
+
 function registerUser(userData) { 
-  const url = 'http://localhost:3000/signup';
-  return axios.post(url, userData);
+  // return axios.post('http://localhost:3000/signup', userData);
+  // 공통 설정으로 더 단순하게 변경
+  return instance.post('signup', userData)
 }
 
 export { registerUser };

4c515feb14fb2801a3dac0cdbc8a2e541a18c531
taykim7 <th_77@naver.com>
2024-01-01 22:59:18 +0900

    로그인시 헤더 분기처리

--- vue-endgame/vue-til/src/components/common/AppHeader.vue
+++ vue-endgame/vue-til/src/components/common/AppHeader.vue
@@ -6,16 +6,26 @@
       </router-link>
     </div>
     <div class="navigations">
-      <span>{{ $store.state.username }}</span>
-      <router-link to="/login">로그인</router-link>
-      <router-link to="/signup">회원가입</router-link>
+      <!-- 로그인 분기1 -->
+      <template v-if='isUserLogin'>
+        <span>{{ $store.state.username }}</span>
+      </template>
+      <!-- 로그인 분기2 -->
+      <template v-else>
+        <router-link to="/login">로그인</router-link>
+        <router-link to="/signup">회원가입</router-link>
+      </template>
     </div>
   </header>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    isUserLogin() {
+      return this.$store.getters.isLogin
+    }
+  }
 }
 </script>
 
--- vue-endgame/vue-til/src/store/index.js
+++ vue-endgame/vue-til/src/store/index.js
@@ -7,6 +7,12 @@ export default new Vuex.Store({
   state: {
     username: '',
   },
+  // computed 랑 비슷
+  getters: {
+    isLogin(state) {
+      return state.username !== '';
+    }
+  },
   mutations: {
     setUsername(state, username) { 
       state.username = username;

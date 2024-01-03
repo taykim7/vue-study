fc497a9dab3a5a7ad86585f9f828a5126cb3e56d
taykim7 <th_77@naver.com>
2024-01-03 19:34:17 +0900

    데이터로딩(스피너) 추가

--- vue-endgame/vue-til/src/views/MainPage.vue
+++ vue-endgame/vue-til/src/views/MainPage.vue
@@ -2,7 +2,8 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
-      <ul>
+      <LoadingSpinner v-if="isLoading"></LoadingSpinner>
+      <ul v-else>
         <PostListItem
           v-for="postItem in postItems"
           :key="postItem._id"
@@ -16,19 +17,24 @@
 <script>
 import { fetchPosts } from '@/api/index';
 import PostListItem from '../components/posts/PostListItem.vue';
+import LoadingSpinner from '../components/common/LoadingSpinner.vue';
 
 export default {
   components: {
     PostListItem,
+    LoadingSpinner,
   },
   data() {
     return {
       postItems: [],
+      isLoading: false,
     };
   },
   methods: {
     async fetchData() {
+      this.isLoading = true;
       const { data } = await fetchPosts();
+      this.isLoading = false;
       console.log(data);
       this.postItems = data.posts;
     }

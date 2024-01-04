5bf95d369b044d450890d49a27c48a756bcfb277
taykim7 <th_77@naver.com>
2024-01-04 23:15:57 +0900

    글작성 페이지 추

---
+++ vue-endgame/vue-til/src/components/posts/PostAddForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="contents">
+    <h1 class="page-header">게시글 작성</h1>
+    <div class="form-wrapper">
+      <form class="form" @submit.prevent="submitForm">
+        <div>
+          <label for="title">Title: </label>
+          <input id="title" type="text" v-model="title">
+        </div>
+        <div>
+          <label for="contents">Contents: </label>
+          <textarea id="contents" type="text" rows="5" v-model="contents"/>
+        </div>
+        <button class="btn" type="submit">생성</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      contents: '',
+    }
+  },
+  methods: {
+    submitForm() {
+      alert('등록요청')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.form-wrapper .form {
+  width: 100%;
+}
+.btn {
+  color: white;
+}
+</style>
---
+++ vue-endgame/vue-til/src/views/PostAddPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="form-container">
+    <PostAddForm></PostAddForm>
+  </div>
+</template>
+
+<script>
+import PostAddForm from '../components/posts/PostAddForm.vue'
+export default {
+  components: {
+    PostAddForm,
+  },
+}
+</script>
+
+<style>
+
+</style>

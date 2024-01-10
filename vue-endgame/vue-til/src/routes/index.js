190aee136d47717229e7c34e4a8427b1154fb07d
taykim7 <th_77@naver.com>
2024-01-10 19:03:08 +0900

    네비게이션 가드 추가
    - to : 이동하려는 페이지
    - from : 현재 페이지
    - next : 페이지 이동할 때 호출하는 API (next()를 해야만 이동한다.)
    - meta의 auth를 추가해서 인증이 필요한 경우를 구분
    - isLogin을 통해 로그인된 상태인지 구분(쿠키)
    - auth가 true인데 로그인아 안되어있으면 로그인 페이지로 이동

--- vue-endgame/vue-til/src/routes/index.js
+++ vue-endgame/vue-til/src/routes/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '@/store/index'
 
 // 코드 스플리팅
 // import LoginPage from '../views/LoginPage.vue'
@@ -7,7 +8,7 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
   // url에 # 제거
   mode: 'history',
   routes: [
@@ -26,16 +27,19 @@ export default new VueRouter({
     },
     {
       path: '/main',
-      component: ()=> import('@/views/MainPage.vue'),
+      component: () => import('@/views/MainPage.vue'),
+      meta: { auth: true },
     },
     {
       path: '/add',
       component: ()=> import('@/views/PostAddPage.vue'),
+      meta: { auth: true },
     },
     {
       // Dynamic Route Matching
       path: '/post/:id',
       component: ()=> import('@/views/PostEditPage.vue'),
+      meta: { auth: true },
     },
     {
       // 라우터 폴백
@@ -44,3 +48,19 @@ export default new VueRouter({
     }
   ]
 });
+
+// 네비게이션 가드
+// to : 이동하려는 페이지
+// from : 현재 페이지
+// next : 페이지 이동할 때 호출하는 API (next()를 해야만 이동한다.)
+router.beforeEach((to, from, next) => { 
+  if (to.meta.auth && !store.getters.isLogin) { 
+    // 인증이 필요한 페이지일 경우 (auth)
+    // + 로그인하지 않았을 때 (!isLogin)
+    next('/login');
+    return;
+  }
+  next();
+})
+
+export default router;

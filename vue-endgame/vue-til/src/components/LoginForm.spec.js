97240b90f54186ff205cb1da2cab5d9ec727ca48
taykim7 <th_77@naver.com>
2024-01-11 19:34:56 +0900

    코드 정리

--- vue-endgame/vue-til/src/components/LoginForm.spec.js
+++ vue-endgame/vue-til/src/components/LoginForm.spec.js
@@ -1,5 +1,9 @@
 // import { sum } from './math';
+// import Vue from 'vue';
+import LoginForm from './LoginForm.vue';
+import { shallowMount } from '@vue/test-utils';
 
+// 1. 테스트용 테스트
 // describe('math.js', () => {
 //   test('10+20=30', () => {
 //     const result = sum(10, 20);
@@ -9,17 +13,20 @@
 //   });
 // });
 
-// import Vue from 'vue';
-import LoginForm from './LoginForm.vue';
-import { shallowMount } from '@vue/test-utils';
+// 2. 기본 테스트
+// describe('LoginForm.vue', () => {
+//   test('컴포넌트가 마운팅되면 unsername이 존재', () => {
+//     const instance = new Vue(LoginForm).$mount();
+//     expect(instance.username).toBe('');
+//   });
+// });
 
-describe('LoginForm.vue', () => {
-  test('컴포넌트가 마운팅되면 unsername이 존재', () => {
-    // const instance = new Vue(LoginForm).$mount();
-    // expect(instance.username).toBe('');
+// 3. test-utils 활용한 테스트
+// describe('LoginForm.vue', () => {
+//   test('컴포넌트가 마운팅되면 unsername이 존재', () => {
+//     const wrapper = shallowMount(LoginForm);
+//     expect(wrapper.vm.username).toBe('');
+//   });
+// });
 
-    // test-utils 활용
-    const wrapper = shallowMount(LoginForm);
-    expect(wrapper.vm.username).toBe('');
-  });
-});
+// ================================

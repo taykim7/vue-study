967543922348234a32238a439876a106e7d61a43
taykim7 <th_77@naver.com>
2024-01-11 19:55:22 +0900

    컴포넌트 테스트
    - shallowMount에 data를 만들 수 있음
    - 특정 html 태그를 찾는 find 활용
    - 컴포넌트의 computed 값 (isUsernameValid) 확인 가능
    - 이메일 형식이 아닐 경우 warning class가 생겨서 .warning을 확인가능
    - 테스트1은 이메일 형식 확인 테스트
    - 테스트2는 입력여부 확인하여 비활성화 테스트

--- vue-endgame/vue-til/src/components/LoginForm.spec.js
+++ vue-endgame/vue-til/src/components/LoginForm.spec.js
@@ -30,3 +30,42 @@ import { shallowMount } from '@vue/test-utils';
 // });
 
 // ================================
+
+// shallowMount에 data를 만들 수 있음
+// 특정 html 태그를 찾는 find 활용
+// 컴포넌트의 computed 값 (isUsernameValid) 확인
+// 이메일 형식이 아닐 경우 warning class가 생겨서 .warning을 확인
+
+describe('LoginForm.vue', () => {
+  // 테스트1
+  test('ID가 이메일 형식이 아니면 경고메세지가 출력됨', () => {
+    const wrapper = shallowMount(LoginForm, {
+      data() { 
+        return {
+          username: '틀린이메일',
+        };
+      }
+    });
+
+    const idInput = wrapper.find('#username');
+    console.log('input 값 : ', idInput.element.value);
+    console.log('이메일 형식인가요 : ', wrapper.vm.isUsernameValid);
+
+    const warningText = wrapper.find('.warning');
+    expect(warningText.exists()).toBeTruthy();
+  });
+
+  // 테스트2
+  test('id pw 입력하지 않으면 로그인 버튼 비활성화', () => {
+    const wrapper = shallowMount(LoginForm, {
+      data() { 
+        return {
+          username: '',
+          password: '',
+        };
+      }
+    });
+    const button = wrapper.find('.btn');
+    expect(button.element.disabled).toBeTruthy();
+  });
+});

40d178a89f1663516e86a1f0239fec181b4d5416
taykim7 <th_77@naver.com>
2024-01-11 19:28:19 +0900

    컴포넌트 테스트

--- vue-endgame/vue-til/src/components/LoginForm.spec.js
+++ vue-endgame/vue-til/src/components/LoginForm.spec.js
@@ -1,10 +1,20 @@
-import { sum } from './math';
+// import { sum } from './math';
 
-describe('math.js', () => {
-  test('10+20=30', () => {
-    const result = sum(10, 20);
+// describe('math.js', () => {
+//   test('10+20=30', () => {
+//     const result = sum(10, 20);
 
-    // 30일거라고 예상한다
-    expect(result).toBe(30);
+//     // 30일거라고 예상한다
+//     expect(result).toBe(30);
+//   });
+// });
+
+import Vue from 'vue';
+import LoginForm from './LoginForm.vue';
+
+describe('LoginForm.vue', () => {
+  test('컴포넌트가 마운팅되면 unsername이 존재', () => {
+    const instance = new Vue(LoginForm).$mount();
+    expect(instance.username).toBe('');
   });
 });

f7b43595a412fb1df20da25f444352eb9083c803
taykim7 <th_77@naver.com>
2024-02-20 18:59:00 +0900

    input 컴포넌트 등록

--- vue-typescript/vue-todo/src/App.vue
+++ vue-typescript/vue-todo/src/App.vue
@@ -1,29 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <h1>Vue Todo with Typescript</h1>
+    <todo-input></todo-input>
   </div>
 </template>
 
-<script lang="ts">
+<script lan="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import TodoInput from "./components/TodoInput.vue";
 
 export default Vue.extend({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: { TodoInput },
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

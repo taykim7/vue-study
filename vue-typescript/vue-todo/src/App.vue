2e5936c5705cdea2826fc86828327ac247141857
taykim7 <th_77@naver.com>
2024-02-22 19:43:41 +0900

    할일 완료 기능 구현
    - props로 데이터를 수신할 경우 타입은 PropType 제네릭 타입으로 명시
    - 데이터 변경할 경우 수신한 데이터를 하나씩 정의하기보다 스프레드 오퍼레이터 (...)를 활용할 수 있다.

--- vue-typescript/vue-todo/src/App.vue
+++ vue-typescript/vue-todo/src/App.vue
@@ -16,6 +16,7 @@
             :key="index"
             :todoItem="todoItem"
             :index="index"
+            @toggle="toggleTodoItemComplete"
             @remove="removeTodoItem"
           ></TodoListItem>
         </ul>
@@ -35,7 +36,7 @@ const STORAGE_KEY = "vue-todo-ts-v1";
 // 스토리지 관련 함수 정의
 const storage = {
   // 저장
-  save(todoItems: any[]) {
+  save(todoItems: Todo[]) {
     const parsed = JSON.stringify(todoItems);
     // key, value 로 저장
     localStorage.setItem(STORAGE_KEY, parsed);
@@ -49,12 +50,17 @@ const storage = {
   },
 };
 
+export interface Todo {
+  title: string;
+  done: boolean;
+}
+
 export default Vue.extend({
   components: { TodoInput, TodoListItem },
   data() {
     return {
       todoText: "",
-      todoItems: [] as any,
+      todoItems: [] as Todo[],
     };
   },
   methods: {
@@ -66,8 +72,12 @@ export default Vue.extend({
     addTodoItem() {
       // 입력된 값 저장
       const value = this.todoText;
+      const todo: Todo = {
+        title: value,
+        done: false,
+      };
       // 배열에 추가
-      this.todoItems.push(value);
+      this.todoItems.push(todo);
       // 배열을 스토리지에 저장
       storage.save(this.todoItems);
       // 초기화
@@ -86,6 +96,18 @@ export default Vue.extend({
       console.log("remove", index);
       // 삭제하고 저장하기
       this.todoItems.splice(index, 1);
+      storage.save(this.todoItems);
+    },
+    // 완료하기 (토글)
+    toggleTodoItemComplete(item: Todo, index: number) {
+      // splice를 통해 해당 index 요소 1개 제거하고 세번째 파라미터로 교환
+      this.todoItems.splice(index, 1, {
+        // 교환할 데이터는 Todo 타입이라 Todo 타입 요소 하나씩 넣어야 하지만
+        // title: item.title, done: !item.done,
+        // 스프레드 오퍼레이터를 통해 다 가져오고 변경할 것만 변경
+        ...item,
+        done: !item.done,
+      });
       storage.save(this.todoItems);
     },
   },
--- vue-typescript/vue-todo/src/components/TodoListItem.vue
+++ vue-typescript/vue-todo/src/components/TodoListItem.vue
@@ -1,7 +1,7 @@
 <template>
   <li>
-    <span>
-      {{ todoItem }}
+    <span class="item" :class="todoItemClass" @click="toggleItem">
+      {{ todoItem.title }}
     </span>
     <!-- <button @click="$emit('delete')">삭제</button> -->
     <button @click="removeTodo">삭제</button>
@@ -9,14 +9,25 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue, { PropType } from "vue";
+import { Todo } from "../App.vue";
 export default Vue.extend({
   props: {
     // props로 수신
-    todoItem: String,
+    // props 타입 정의 (PropType)
+    todoItem: Object as PropType<Todo>,
     index: Number,
   },
+  computed: {
+    // computed의 반환 타입 꼭 넣자
+    todoItemClass(): string | null {
+      return this.todoItem.done ? "complete" : null;
+    },
+  },
   methods: {
+    toggleItem() {
+      this.$emit("toggle", this.todoItem, this.index);
+    },
     removeTodo() {
       // emit으로 전송
       this.$emit("remove", this.index);
@@ -24,3 +35,11 @@ export default Vue.extend({
   },
 });
 </script>
+<style scoped>
+.item {
+  cursor: pointer;
+}
+.complete {
+  text-decoration: line-through;
+}
+</style>

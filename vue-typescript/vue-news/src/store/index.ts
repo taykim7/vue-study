6ca368f298d4856a4b17ed5271e922d8af106b1f
taykim7 <th_77@naver.com>
2024-02-25 21:37:06 +0900

    수정

--- vue-typescript/vue-news/src/store/index.ts
+++ vue-typescript/vue-news/src/store/index.ts
@@ -3,7 +3,7 @@ import Vuex, { StoreOptions } from "vuex";
 import { RootState, state } from "./state";
 import { mutations } from "./mutations";
 import { actions } from "./actions";
-import getters from "./getters";
+import { getters } from "./getters";
 
 Vue.use(Vuex);
 
@@ -26,6 +26,7 @@ const store: StoreOptions<RootState> = {
   state: state,
   mutations: mutations,
   actions: actions,
+  getters: getters,
 };
 
 export default new Vuex.Store(store);

eb73ba0d6f5a9ccba92f4570fb12f6bcba8bd1cb
taykim7 <th_77@naver.com>
2024-03-12 19:40:11 +0900

    props, emit

---
+++ vue3-starter/vue3-cli/src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{ appTitle }}</p>
+    <button @click="changeTitle">버튼</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['appTitle'],
+  methods: {
+    changeTitle() {
+      this.$emit('tryChange');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

78f36d7f5d59746c877c25e1d513f6edb31fd5ba
taykim7 <th_77@naver.com>
2024-03-13 18:22:56 +0900

    Composition api 로 변환

--- vue3-starter/vue3-form/src/App.vue
+++ vue3-starter/vue3-form/src/App.vue
@@ -16,32 +16,59 @@
 
 <script>
 import axios from 'axios';
+import { ref } from 'vue';
 
 export default {
-data() {
-  return {
-    username: '',
-    password: '',
-  }
-},
-methods: {
-  submitForm() {
-    // submit의 기본 동작인 새로고침을 제어
-    // event.preventDefault();
-    // 이벤트 대신에 @submit.prevent 로 똑같이 막을 수 있음.
+  setup() {
+    // data
+    var username = ref('');
+    var password = ref('');
 
-    const data = {
-      username: this.username,
-      password: this.password,
+    // methods
+    var submitForm = () => {
+      // fake api
+      axios.post('https://jsonplaceholder.typicode.com/users/', {
+        username: username.value,
+        password: password.value,
+      })
+        .then(response => {
+          console.log(response);
+        })
     }
-    // fake api
-    axios.post('https://jsonplaceholder.typicode.com/users/', data)
-    .then(response => {
-      console.log(response);
-    })
+
+    // 사용하고자하는 것들을 인스턴스 옵션으로 보냄
+    return {
+      username,
+      password,
+      submitForm
+    }
   }
-}  
+
+  // data() {
+  //   return {
+  //     username: '',
+  //     password: '',
+  //   }
+  // },
+  // methods: {
+  //   submitForm() {
+  //     // submit의 기본 동작인 새로고침을 제어
+  //     // event.preventDefault();
+  //     // 이벤트 대신에 @submit.prevent 로 똑같이 막을 수 있음.
+
+  //     const data = {
+  //       username: this.username,
+  //       password: this.password,
+  //     }
+  //     // fake api
+  //     axios.post('https://jsonplaceholder.typicode.com/users/', data)
+  //     .then(response => {
+  //       console.log(response);
+  //     })
+  //   }
+  //   }
 }
+
 </script>
 
 <style scoped>

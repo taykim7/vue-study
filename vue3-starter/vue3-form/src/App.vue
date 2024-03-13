1a1c0e85a47140df542961e030bb39c0f11fd124
taykim7 <th_77@naver.com>
2024-03-13 18:16:44 +0900

    axios 설치, 간단 통신

--- vue3-starter/vue3-form/src/App.vue
+++ vue3-starter/vue3-form/src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form action="" @submit.prevent="submitForm">
+      <div>
+        <label for="userId">아이디:</label>
+        <input id="userId" type="text" v-model="username">
+      </div>
+      <div>
+        <label for="password">비번:</label>
+        <input id="password" type="text" v-model="password">
+      </div>
+      <button type="submit">로그인</button>
+    </form>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+data() {
+  return {
+    username: '',
+    password: '',
   }
+},
+methods: {
+  submitForm() {
+    // submit의 기본 동작인 새로고침을 제어
+    // event.preventDefault();
+    // 이벤트 대신에 @submit.prevent 로 똑같이 막을 수 있음.
+
+    const data = {
+      username: this.username,
+      password: this.password,
+    }
+    // fake api
+    axios.post('https://jsonplaceholder.typicode.com/users/', data)
+    .then(response => {
+      console.log(response);
+    })
+  }
+}  
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>

eb0078aeac9103559b0092930b87dc03ddf392a9
taykim7 <th_77@naver.com>
2024-03-20 18:04:39 +0900

    인풋 기능 구현

--- vue3-middle/vue3-todo/src/App.vue
+++ vue3-middle/vue3-todo/src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <TodoHeader></TodoHeader>
+  <TodoInput></TodoInput>
 </template>
 
 <script>
 import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
 export default {
-  components: { TodoHeader },
+  components: { TodoHeader, TodoInput },
 }
 </script>
---
+++ vue3-middle/vue3-todo/src/components/TodoInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="text" v-model="todoInput">
+    <button @click="addTodo">할일추가</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    // data
+    const todoInput = ref('');
+    const addTodo = () => {
+      const todo = todoInput.value;
+      // key value 동일하게 저장
+      localStorage.setItem(todo, todo);
+    }
+    // methods
+    return {
+      todoInput, addTodo
+    }
+  },
+}
+</script>

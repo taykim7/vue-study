53be8310c5777570b4714ab425be8a987518f0d1
taykim7 <th_77@naver.com>
2024-03-20 18:32:39 +0900

    목록 표시 리팩토링
    - 데이터를 상위에서 props로 전달하는 방식으로 (같은 레벨의 컴포넌트와 통신하기 위해)

--- vue3-middle/vue3-todo/src/App.vue
+++ vue3-middle/vue3-todo/src/App.vue
@@ -1,14 +1,33 @@
 <template>
   <TodoHeader></TodoHeader>
   <TodoInput></TodoInput>
-  <TodoList></TodoList>
+  <TodoList :todoItems="todoItems"></TodoList>
 </template>
 
 <script>
+import { ref } from 'vue'
 import TodoHeader from './components/TodoHeader.vue'
 import TodoInput from './components/TodoInput.vue'
 import TodoList from './components/TodoList.vue'
 export default {
   components: { TodoHeader, TodoInput, TodoList },
+
+  setup() {
+    const todoItems = ref([]);
+
+    const fetchTodos = () => {
+      const result = [];
+      for (let i = 0; i < localStorage.length; i++) {
+        const todoItem = localStorage.key(i);
+        result.push(todoItem);
+      }
+      return result;
+    }
+    todoItems.value = fetchTodos();
+
+    return {
+      todoItems,
+    }
+  }
 }
 </script>

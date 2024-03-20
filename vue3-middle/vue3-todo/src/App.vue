eb8c0a31ed211f7cc0c1a7db95111a553fca84d2
taykim7 <th_77@naver.com>
2024-03-20 18:23:19 +0900

    목록 표시 구현

--- vue3-middle/vue3-todo/src/App.vue
+++ vue3-middle/vue3-todo/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <TodoHeader></TodoHeader>
   <TodoInput></TodoInput>
+  <TodoList></TodoList>
 </template>
 
 <script>
 import TodoHeader from './components/TodoHeader.vue'
 import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
 export default {
-  components: { TodoHeader, TodoInput },
+  components: { TodoHeader, TodoInput, TodoList },
 }
 </script>
---
+++ vue3-middle/vue3-todo/src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in items" :key="index">
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import { ref } from 'vue'
+  export default {
+  setup() {
+      const items = ref([]);
+      const fetchTodos = () => {
+        for (let i = 0; i < localStorage.length; i++) {
+          const todoItem = localStorage.key(i);
+          items.value.push(todoItem);
+        }
+      }
+      // 새로 갱신은 되지 않는다.
+      fetchTodos();
+      return { items, fetchTodos }
+    }
+  }
+</script>

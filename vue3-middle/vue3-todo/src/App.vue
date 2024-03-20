1d2a7295cec090b021deda3bb24ed45f1103c78f
taykim7 <th_77@naver.com>
2024-03-20 17:56:46 +0900

    헤더 등록

--- vue3-middle/vue3-todo/src/App.vue
+++ vue3-middle/vue3-todo/src/App.vue
@@ -1,26 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TodoHeader></TodoHeader>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoHeader from './components/TodoHeader.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { TodoHeader },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ vue3-middle/vue3-todo/src/components/TodoHeader.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>TO DO HEADER</h1>
+</template>

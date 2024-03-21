94df9618eb3a118db4834ee511a46f19500a999b
taykim7 <th_77@naver.com>
2024-03-21 18:53:18 +0900

    watch는 지양하자

--- vue3-middle/vue3-todo/src/components/TodoList.vue
+++ vue3-middle/vue3-todo/src/components/TodoList.vue
@@ -8,12 +8,20 @@
 </template>
 
 <script>
+import { watch } from 'vue';
 export default {
   props: ['todoItems'],
   setup(props, context) {
     function removeTodo(item, index) {
       context.emit('remove', item, index);
     }
+
+    // todoItems가 변할때 마다 동작
+    watch(props.todoItems, (newValue) => {
+      console.log({ newValue });
+      // 하지만 최대한 watch는 지양하자.. 추적이 어렵기 때문에!
+      // 명시적인 코드를 활용하자
+    })
     return { removeTodo }
   }
 }

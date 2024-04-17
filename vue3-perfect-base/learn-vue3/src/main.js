619b639ad40b7335846e4c2ffb7ef05fd3f5b3c4
taykim7 <th_77@naver.com>
2024-04-17 18:42:17 +0900

    .

--- vue3-perfect-base/learn-vue3/src/main.js
+++ vue3-perfect-base/learn-vue3/src/main.js
@@ -1,8 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/main.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
-
+// app.component('AppCard', AppCard);
 createApp(App).mount('#app');
 import 'bootstrap/dist/js/bootstrap.js';

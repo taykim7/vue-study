06c4dbe0b2b267d14e251fa5c22b0368037a4e24
taykim7 <th_77@naver.com>
2024-04-19 23:35:44 +0900

    Slots

--- vue3-perfect-base/learn-vue3/src/components/AppCard.vue
+++ vue3-perfect-base/learn-vue3/src/components/AppCard.vue
@@ -1,62 +1,31 @@
 <template>
 	<div class="card">
-		<div class="card-body">
-			<!-- type : news , notice -->
-			<span class="badge bg-secondary">{{ typeName }}</span>
-			<h5 class="card-title red mt-2">{{ title }}</h5>
-			<p class="card-text">{{ contents }}</p>
-			<a href="#" class="btn" :class="isLikeClass" @click="toggleLike">
-				좋아요
-			</a>
+		<!-- header라는 슬롯이 정의되어야 렌더하겠다. -->
+		<div v-if="$slots.header" class="card-header">
+			<slot name="header" header-message="자식의 헤더 메세지">header 슬롯</slot>
+		</div>
+		<!-- header라는 슬롯이 정의되어야 렌더하겠다. -->
+		<div v-if="$slots.default" class="card-body">
+			<slot :child-message="childMessage">body 슬롯</slot>
+		</div>
+		<!-- header라는 슬롯이 정의되어야 렌더하겠다. -->
+		<div v-if="hasFooter" class="card-footer text-muted">
+			<slot name="footer" footer-message="자식의 푸터 메세지">footer 슬롯</slot>
 		</div>
 	</div>
 </template>
 
 <script>
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
 
 export default {
-	props: {
-		type: {
-			type: String,
-			default: 'news',
-			validator: value => {
-				return ['news', 'notice'].includes(value);
-			},
-		},
-		title: {
-			type: String,
-			required: true,
-		},
-		contents: {
-			type: String,
-			// required: true,
-		},
-		isLike: {
-			type: Boolean,
-			default: false,
-		},
-		obj: {
-			type: Object,
-			default: () => ({}),
-		},
-	},
-	emits: ['toggleLike'],
-	setup(props, context) {
-		// console.log('props.title: ', props.title);
-		const isLikeClass = computed(() =>
-			props.isLike ? 'btn-danger' : 'btn-outline-danger',
-		);
-		const typeName = computed(() =>
-			props.type === 'news' ? '뉴스' : '공지사항',
-		);
-		const toggleLike = () => {
-			// props.isLike = !props.isLike;
-			context.emit('toggleLike');
-		};
-		return { isLikeClass, typeName, toggleLike };
+	setup(props, { slots }) {
+		const childMessage = ref('자식에서 작성한 메시지');
+		// setup에서 slot접근, footer가 있ㄴ느지 확인
+		const hasFooter = computed(() => !!slots.footer);
+		return { childMessage, hasFooter };
 	},
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped></style>
--- vue3-perfect-base/learn-vue3/src/components/TheView.vue
+++ vue3-perfect-base/learn-vue3/src/components/TheView.vue
@@ -4,6 +4,46 @@
 			<!-- <MyButton @click="sayHello"></MyButton> -->
 			<MyButton @click="sayHello"></MyButton>
 			<LabelInput label="이름"></LabelInput>
+
+			<hr />
+
+			<!-- slot 적용 -->
+			<FancyButton>
+				클릭해라
+				<span style="color: red">슬롯버튼</span>
+			</FancyButton>
+			<FancyButton></FancyButton>
+			<FancyButton v-slot="{ fancyMessage }">{{ fancyMessage }}</FancyButton>
+
+			<hr />
+
+			<AppCard>
+				<!-- 1. slot 기본 사용 -->
+				<!-- <template v-slot:header>제목이요</template>
+				<template v-slot:default>디폴트 슬롯</template>
+				<template v-slot:footer>푸터요</template> -->
+
+				<!--  2. slot 단축표현 사용 '#' -->
+				<!-- <template #header>제목이요</template> -->
+
+				<!-- 3. slot 동적으로 사용 -->
+				<!-- <template #[slotArgs]>동적인 제목이요</template> -->
+
+				<!-- 4. default 는 암시적으로 사용 가능 -->
+				<!-- 암시적인 디폴트 슬롯 -->
+
+				<!-- 5. 부모 -> 자식 slot으로 데이터 전달 -->
+				<!-- <template #default>{{ message }}</template> -->
+
+				<!-- 6. 자식 slot에서 작성한 메세지 수신 -->
+				<!-- <template #header="{ headerMessage }">{{ headerMessage }}</template>
+				<template #default="{ childMessage }">{{ childMessage }}</template>
+				<template #footer="{ footerMessage }">{{ footerMessage }}</template> -->
+
+				<!-- 7. v-if="$slots.header" 를 통해 선택적으로 렌더 -->
+				<!-- 8. 자식컴포넌트에서 setup의 slot에 접근하여 footer 확인 -->
+				<template #default="{ childMessage }">{{ childMessage }}</template>
+			</AppCard>
 		</div>
 	</main>
 </template>
@@ -11,11 +51,16 @@
 <script>
 import MyButton from './MyButton.vue';
 import LabelInput from './LabelInput.vue';
+import FancyButton from './FancyButton.vue';
+import AppCard from './AppCard.vue';
+import { ref } from 'vue';
 
 export default {
 	components: {
 		MyButton,
 		LabelInput,
+		FancyButton,
+		AppCard,
 	},
 	setup() {
 		// click 이벤트도 자식컴포넌트에 상속된다
@@ -26,7 +71,10 @@ export default {
 			// 자식에서는 inheritAttrs 설정을 안했을 때
 			// 자식의 root에 바인딩되서 버튼, div에 대한 이벤트가 발생한다.
 		};
-		return { sayHello };
+		const slotArgs = ref('header');
+		const message = ref('상위에서 작성한 메세지');
+
+		return { sayHello, slotArgs, message };
 	},
 };
 </script>

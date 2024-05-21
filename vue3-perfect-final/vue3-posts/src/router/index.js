7d7de73324c555cbdac61b885b54540b4b30bb02
taykim7 <th_77@naver.com>
2024-05-21 20:03:45 +0900

    router 등록. name 속성 추가

--- vue3-perfect-final/vue3-posts/src/router/index.js
+++ vue3-perfect-final/vue3-posts/src/router/index.js
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import HomeView from '@/views/HomeView.vue';
 import AboutView from '@/views/AboutView.vue';
+import PostCreateView from '@/views/posts/PostCreateView.vue';
+import PostDetailView from '@/views/posts/PostDetailView.vue';
+import PostListView from '@/views/posts/PostListView.vue';
+import PostEditView from '@/views/posts/PostEditView.vue';
 
 const routes = [
 	{
@@ -13,6 +17,26 @@ const routes = [
 		path: '/about',
 		name: 'About',
 		component: AboutView,
+	},
+	{
+		path: '/posts',
+		name: 'PostList',
+		component: PostListView,
+	},
+	{
+		path: '/posts/create',
+		name: 'PostCreate',
+		component: PostCreateView,
+	},
+	{
+		path: '/posts/:id',
+		name: 'PostDetail',
+		component: PostDetailView,
+	},
+	{
+		path: '/posts/:id/edit',
+		name: 'PostEdit',
+		component: PostEditView,
 	},
 ];
 

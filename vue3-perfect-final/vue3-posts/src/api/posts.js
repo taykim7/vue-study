4ee09fbbde1e7fffed4ba20d073b56cf6c8f2a47
taykim7 <th_77@naver.com>
2024-05-21 20:02:28 +0900

    임시 api

---
+++ vue3-perfect-final/vue3-posts/src/api/posts.js
@@ -0,0 +1,12 @@
+// axios로 json 데이터 조회
+const posts = [
+	{ id: 1, title: '제목1', content: '내용1', createdAt: '2020-01-01' },
+	{ id: 2, title: '제목2', content: '내용2', createdAt: '2020-02-02' },
+	{ id: 3, title: '제목3', content: '내용3', createdAt: '2020-03-03' },
+	{ id: 4, title: '제목4', content: '내용4', createdAt: '2020-04-04' },
+	{ id: 5, title: '제목5', content: '내용5', createdAt: '2020-05-05' },
+];
+
+export function getPosts() {
+	return posts;
+}

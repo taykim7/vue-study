a098f73e21f2060e05b925b9161103677591842a
taykim7 <th_77@naver.com>
2024-05-27 22:59:41 +0900

    history 옵션의 hash 모드
    - router 객체의 history 옵션 확인

--- vue3-perfect-final/vue3-posts/src/router/index.js
+++ vue3-perfect-final/vue3-posts/src/router/index.js
@@ -1,4 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {
+	createRouter,
+	// createWebHashHistory,
+	createWebHistory,
+} from 'vue-router';
 
 import HomeView from '@/views/HomeView.vue';
 import AboutView from '@/views/AboutView.vue';
@@ -73,6 +77,7 @@ const routes = [
 
 const router = createRouter({
 	history: createWebHistory('/'),
+	// history: createWebHashHistory(),
 	routes,
 });
 

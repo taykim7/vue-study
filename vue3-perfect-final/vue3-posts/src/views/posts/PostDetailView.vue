45a3bb10123b4f82f9c58efa04df661e69be5778
taykim7 <th_77@naver.com>
2024-05-28 20:02:29 +0900

    상세 조회, 삭제 api 적용
    - 원하는 데이터만 받아오는 setPost 메서드 생성
    - 상세 api 호출 후 setPost에 의해 데이터 가져옴
    - 안티패턴을 활용하여 뎁스가 깊어지거나 가독성을 고려하는 방법

--- vue3-perfect-final/vue3-posts/src/views/posts/PostDetailView.vue
+++ vue3-perfect-final/vue3-posts/src/views/posts/PostDetailView.vue
@@ -3,9 +3,9 @@
 		<!-- <p>params는 {{ $route.params }}</p>
 		<p>query는 {{ $route.query }}</p>
 		<p>hash는 {{ $route.hash }}</p> -->
-		<h2>{{ form.title }}</h2>
-		<p>{{ form.content }}</p>
-		<p class="text-muted">{{ form.createdAt }}</p>
+		<h2>{{ post.title }}</h2>
+		<p>{{ post.content }}</p>
+		<p class="text-muted">{{ post.createdAt }}</p>
 		<hr class="my-4" />
 		<div class="row g-2">
 			<div class="col-auto">
@@ -24,7 +24,7 @@
 				</button>
 			</div>
 			<div class="col-auto">
-				<button class="btn btn-outline-danger">삭제</button>
+				<button class="btn btn-outline-danger" @click="remove">삭제</button>
 			</div>
 		</div>
 	</div>
@@ -35,7 +35,7 @@ import {
 	// useRoute,
 	useRouter,
 } from 'vue-router';
-import { getPostById } from '@/api/posts';
+import { getPostById, deletePost } from '@/api/posts';
 import { ref } from 'vue';
 
 // props: true 라우터 설정으로 props를 받아옴
@@ -59,18 +59,44 @@ const router = useRouter();
  * reactive는 레퍼런스만 가능
  */
 
-const form = ref({});
+const post = ref({});
 // const form = reactive({});
 // form.title = data.title;
 // form.content = data.content;
 
-const fetchPost = () => {
-	const data = getPostById(props.id);
-	// 객체복사 (주소값을 참조해서 데이터가 변경되도 함께 변함)
-	form.value = { ...data };
+const fetchPost = async () => {
+	try {
+		const { data } = await getPostById(props.id);
+		// 객체복사 (주소값을 참조해서 데이터가 변경되도 함께 변함)
+		setPost(data);
+	} catch (error) {
+		console.log(error);
+	}
 };
+
+// 원하는 데이터만 받도록 설정 (다른 데이터로 문제가 생길 수 있으니!)
+const setPost = ({ title, content, createdAt }) => {
+	post.value.title = title;
+	post.value.content = content;
+	post.value.createdAt = createdAt;
+};
+
 fetchPost();
 
+// 제거
+const remove = async () => {
+	try {
+		// 안티패턴 - 뎁스가 깊어지지 않도록(컨벤션에 따라 다름)
+		// 다른 예로 ! 를 === true 이런식으로 교체 가능 (훨씬 직관적이니까)
+		if (confirm('삭제할래?') === false) {
+			return;
+		}
+		await deletePost(props.id);
+		router.push({ name: 'PostList' });
+	} catch (error) {
+		console.log(error);
+	}
+};
 const goListPage = () => router.push({ name: 'PostList' });
 const goEditPage = () =>
 	router.push({ name: 'PostEdit', params: { id: props.id } });

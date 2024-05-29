a5da3bf09cda7c9d3200a206d4579cc18d06519b
taykim7 <th_77@naver.com>
2024-05-29 22:40:43 +0900

    Pagination   (1) 전체 게시글 개수 파악   (2) 한 페이지에 보여줄 개수 설정   (3) 페이지 개수 계산 ( 페이지 수 = 전체 게시글 개수 / 한 페이지에 보여줄 개수 ) + 올림 처리   (4) v-for 페이지 개수로 페이지 표현   (5) 현재 페이지 설정 ( 1 페이지 ) 현재 페이지는 active 표현   (6) 표현된 페이지에 현재 페이지로 설정 (메서드로 설정하던지 알아서)   (7) 이전/다음 페이지 버튼 구현     '이전 페이지 버튼'은 현재 페이지가 1보다 크지 않으면 disabled     '다음 페이지 버튼'은 현재 페이지가 전체 페이지보다 작지 않으면 disbled

--- vue3-perfect-final/vue3-posts/src/views/posts/PostListView.vue
+++ vue3-perfect-final/vue3-posts/src/views/posts/PostListView.vue
@@ -12,7 +12,36 @@
 				></PostItem>
 			</div>
 		</div>
-		<hr class="my-4" />
+		<nav class="mt-5" aria-label="Page navigation example">
+			<ul class="pagination justify-content-center">
+				<li class="page-item">
+					<a
+						class="page-link"
+						:class="{ disabled: !(params._page > 1) }"
+						href="#"
+						@click.prevent="params._page--"
+						>Previous</a
+					>
+				</li>
+				<li
+					v-for="page in pageCount"
+					:key="page"
+					class="page-item"
+					:class="{ active: params._page === page }"
+				>
+					<a class="page-link" href="#" @click.prevent="params._page = page">{{
+						page
+					}}</a>
+				</li>
+				<li
+					class="page-item"
+					:class="{ disabled: !(params._page < pageCount) }"
+				>
+					<a class="page-link" href="#" @click.prevent="params._page++">Next</a>
+				</li>
+			</ul>
+		</nav>
+		<hr class="my-5" />
 		<AppCard>
 			<!-- router의 파라미터에 의존적이었지만 props: true 라우터 설정으로 속성을 전달할 수 있게 되었따 -->
 			<PostDetailView :id="1"></PostDetailView>
@@ -25,17 +54,26 @@ import PostItem from '@/components/posts/PostItem.vue';
 import PostDetailView from '@/views/posts/PostDetailView.vue';
 import AppCard from '@/components/AppCard.vue';
 import { getPosts } from '@/api/posts';
-import { ref } from 'vue';
+import { ref, computed, watchEffect } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
 const posts = ref([]);
-// 정렬추가
+// 데이터 조회 필터
 const params = ref({
-	// 등록일 기준 내림차순
+	// (1) 정렬 기준 등록일 기준, 내림차순
 	_sort: 'createdAt',
 	_order: 'desc',
+	// (2) 조회 개수
+	_limit: 3,
+	// (3) 페이지
+	_page: 1,
 });
+// pagination - 페이지 수 = 전체개수/조회개수 (올림)
+const totalCount = ref(0);
+const pageCount = computed(() =>
+	Math.ceil(totalCount.value / params.value._limit),
+);
 
 const fetchPosts = async () => {
 	// getPosts()
@@ -48,14 +86,19 @@ const fetchPosts = async () => {
 
 	try {
 		// data를 post에 가져오기
-		const { data } = await getPosts(params.value);
+		const { data, headers } = await getPosts(params.value);
 		// ({ data: posts.value } = await getPosts()); // 다른 표현 방법
 		posts.value = data;
+		totalCount.value = headers['x-total-count'];
+		console.log(pageCount.value);
 	} catch (error) {
 		console.log(error);
 	}
 };
-fetchPosts();
+
+// 페이지 변경시 새로 호출 (반응형 데이터 변경 시 새로 호출)
+// watch와 다르게 초기 1번 바로 실행함
+watchEffect(fetchPosts);
 
 const goPage = id => {
 	// router.push(`/posts/${id}`);

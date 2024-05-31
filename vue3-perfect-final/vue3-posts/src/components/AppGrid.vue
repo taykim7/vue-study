e00cd4263ada770b1daa07e0c96eed71c9bacfc5
taykim7 <th_77@naver.com>
2024-05-31 17:00:00 +0900

    리스트 컴포넌트 등록
    - items을 받음
    - 또한 class명을 받아서 적용할 수 있음 (부트스트랩)

---
+++ vue3-perfect-final/vue3-posts/src/components/AppGrid.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="row g-3">
+		<div v-for="(item, index) in items" :key="index" :class="colClass">
+			<slot :item="item" :index="index"></slot>
+		</div>
+	</div>
+</template>
+
+<script setup>
+defineProps({
+	items: {
+		type: Array,
+		requied: true,
+	},
+	// class
+	colClass: {
+		type: String,
+		default: 'col-4',
+	},
+});
+</script>
+
+<style lang="scss" scoped></style>

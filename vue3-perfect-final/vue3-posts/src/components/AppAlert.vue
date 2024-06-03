15d87faeb7df2d96e90cd6eb6e24064a6e055625
taykim7 <th_77@naver.com>
2024-06-03 18:55:56 +0900

    Transition 컴포넌트 설정
    - class를 통해 애니메이션 조정

--- vue3-perfect-final/vue3-posts/src/components/AppAlert.vue
+++ vue3-perfect-final/vue3-posts/src/components/AppAlert.vue
@@ -1,7 +1,9 @@
 <template>
-	<div v-if="show" class="app-alert alert" :class="styleClass" role="alert">
-		{{ message }}
-	</div>
+	<Transition name="slide">
+		<div v-if="show" class="app-alert alert" :class="typeStyle" role="alert">
+			{{ message }}
+		</div>
+	</Transition>
 </template>
 
 <script setup>
@@ -23,7 +25,7 @@ const props = defineProps({
 	},
 });
 
-const styleClass = computed(() =>
+const typeStyle = computed(() =>
 	props.type === 'error' ? 'alert-danger' : 'alert-primary',
 );
 </script>
@@ -33,5 +35,50 @@ const styleClass = computed(() =>
 	position: fixed;
 	top: 10px;
 	right: 10px;
+}
+/* ============ 나타날 때 ============ */
+/* .v-enter-from {
+	opacity: 0;
+}
+.v-enter-active {
+	transition: opacity 0.5s ease;
+}
+.v-enter-to {
+	opacity: 1;
+} */
+/* ============ 사라질 때 ============ */
+/* .v-leave-from {
+	opacity: 1;
+}
+.v-leave-active {
+	transition: opacity 0.5s ease;
+}
+.v-leave-to {
+	opacity: 0;
+} */
+
+/* ============ 나타날 때 (slide) ============ */
+.slide-enter-from {
+	opacity: 0;
+	transform: translateY(-30px);
+}
+.slide-enter-active {
+	transition: all 0.5s ease;
+}
+.slide-enter-to {
+	opacity: 1;
+	transform: translateY(0px);
+}
+/* ============ 사라질 때 (slide) ============ */
+.slide-leave-from {
+	opacity: 1;
+	transform: translateY(0px);
+}
+.slide-leave-active {
+	transition: all 0.5s ease;
+}
+.slide-leave-to {
+	opacity: 0;
+	transform: translateY(-30px);
 }
 </style>

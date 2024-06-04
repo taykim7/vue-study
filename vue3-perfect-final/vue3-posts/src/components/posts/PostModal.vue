6b160864e41691f995d9abdd4cf2c61575d7fec0
taykim7 <th_77@naver.com>
2024-06-04 19:46:38 +0900

    post용 Modal 컴포넌트 생성
    - AppModal컴포넌트의 slot활용
    - v-model을 show로 등록
    - show는 computed로 gettter setter 등록
    - props 설정
    - getter에 의해 show는 props의 modelValue를 호출
    - setter에 의해 show의 값을 변경하면 update:modelValue emit을 호출

---
+++ vue3-perfect-final/vue3-posts/src/components/posts/PostModal.vue
@@ -0,0 +1,51 @@
+<template>
+	<AppModal v-model="show" :show="show" :title="'게시글입니다'">
+		<template #default>
+			<div class="row g-3">
+				<div class="col-3 text-muted">제목</div>
+				<div class="col-9">{{ title }}</div>
+				<div class="col-3 text-muted">내용</div>
+				<div class="col-9">{{ content }}</div>
+				<div class="col-3 text-muted">등록일</div>
+				<div class="col-9">{{ createdAt }}</div>
+			</div>
+		</template>
+		<template #actions>
+			<button
+				type="button"
+				class="btn btn-secondary"
+				data-bs-dismiss="modal"
+				@click="closeModal"
+			>
+				닫기
+			</button>
+		</template></AppModal
+	>
+</template>
+
+<script setup>
+import AppModal from '@/components/AppModal.vue';
+import { computed } from 'vue';
+const props = defineProps({
+	modalValue: Boolean,
+	title: String,
+	content: String,
+	createdAt: [String, Number],
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const show = computed({
+	get() {
+		return props.modelValue;
+	},
+	set(value) {
+		emit('update:modelValue', value);
+	},
+});
+
+// 닫기
+const closeModal = () => (show.value = false);
+</script>
+
+<style lang="scss" scoped></style>

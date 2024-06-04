0229bffb4d6ffc3f8dd042d05ef3fe89cd87f371
taykim7 <th_77@naver.com>
2024-06-04 19:52:45 +0900

    Modal 애니메이션 Transition 적용
    - 하지만 Modal의 구조 위치에 의해 css가 무너짐

--- vue3-perfect-final/vue3-posts/src/components/AppModal.vue
+++ vue3-perfect-final/vue3-posts/src/components/AppModal.vue
@@ -1,38 +1,40 @@
 <template>
-	<!-- Modal -->
-	<div v-if="modelValue">
-		<div class="modal-backdrop fade show"></div>
-		<div
-			class="modal fade show d-block"
-			tabindex="-1"
-			aria-labelledby="exampleModalLabel"
-			aria-hidden="true"
-		>
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<slot name="header">
-							<h1 class="modal-title fs-5" id="exampleModalLabel">
-								{{ title }}
-							</h1>
-							<button
-								type="button"
-								class="btn-close"
-								aria-label="Close"
-								@click="$emit('update:modelValue', false)"
-							></button>
-						</slot>
-					</div>
-					<div class="modal-body">
-						<slot></slot>
-					</div>
-					<div class="modal-footer">
-						<slot name="actions"> </slot>
+	<Transition>
+		<!-- Modal -->
+		<div v-if="modelValue">
+			<div class="modal-backdrop fade show"></div>
+			<div
+				class="modal fade show d-block"
+				tabindex="-1"
+				aria-labelledby="exampleModalLabel"
+				aria-hidden="true"
+			>
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<slot name="header">
+								<h1 class="modal-title fs-5" id="exampleModalLabel">
+									{{ title }}
+								</h1>
+								<button
+									type="button"
+									class="btn-close"
+									aria-label="Close"
+									@click="$emit('update:modelValue', false)"
+								></button>
+							</slot>
+						</div>
+						<div class="modal-body">
+							<slot></slot>
+						</div>
+						<div class="modal-footer">
+							<slot name="actions"> </slot>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
+	</Transition>
 </template>
 
 <script setup>
@@ -44,4 +46,25 @@ defineProps({
 defineEmits(['close', 'update:modelValue']);
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+/* ============ 나타날 때 ============ */
+.v-enter-from {
+	opacity: 0;
+}
+.v-enter-active {
+	transition: all 0.5s ease;
+}
+.v-enter-to {
+	opacity: 1;
+}
+/* ============ 사라질 때 ============ */
+.v-leave-from {
+	opacity: 1;
+}
+.v-leave-active {
+	transition: all 0.5s ease;
+}
+.v-leave-to {
+	opacity: 0;
+}
+</style>

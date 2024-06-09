814d4fa887b40d163d25882482be61f9ebbbebce
taykim7 <th_77@naver.com>
2024-06-09 17:36:49 +0900

    ○ 플러그인을 활용한 글로벌 컴포넌트   - unplugin-vue-components 플러그인을 사용   - 컴포넌트를 자동으로 import해주는 모듈   - 또한 인터페이스를 자동으로 설정해줌

--- vue3-perfect-final/vue3-posts/vite.config.js
+++ vue3-perfect-final/vue3-posts/vite.config.js
@@ -1,11 +1,20 @@
 import { fileURLToPath, URL } from 'node:url';
+import Components from 'unplugin-vue-components/vite';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		Components({
+			// components.d.ts 파일 생성여부
+			dts: true,
+			// 자동으로 가져오길 원하는 컴포넌트의 경로
+			dirs: ['src/component/app'],
+		}),
+	],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),

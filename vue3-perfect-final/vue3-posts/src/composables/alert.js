943d57189fc30cc2eb7c527f33283ca0ffdcd6b9
taykim7 <th_77@naver.com>
2024-06-11 19:48:32 +0900

    등록이나 수정하여 페이지가 이동하면 Alert가 안나오는 문제 해결 시도2
    - 각 컴포넌트별로 각각의 Alert 인스턴스를 사용하므로 호출되는 시점에서는 Alert의 상태가 초기화된다.
    - 그래서 Alert 컴포저블 내부에서 상태를 함수 범위가 아닌 모듈 범위로 변경
    - 실제론 '상태 관리 모듈' 을 사용한다. ***

--- vue3-perfect-final/vue3-posts/src/composables/alert.js
+++ vue3-perfect-final/vue3-posts/src/composables/alert.js
@@ -1,8 +1,12 @@
 // composable 함수를 만들 땐 대부분 use를 붙힌다.
 import { ref } from 'vue';
 
+// 모듈 범위의 Alert 상태
+const alerts = ref([]);
+
 export function useAlert() {
-	const alerts = ref([]);
+	// 함수 범위의 Alert 상태
+	// const alerts = ref([]);
 	const vAlert = (message, type = 'error') => {
 		alerts.value.push({ message, type });
 		setTimeout(() => {

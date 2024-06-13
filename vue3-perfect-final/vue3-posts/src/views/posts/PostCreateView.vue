a7f38c2b94d04e9b4057862eeaa20de7599eadaf
taykim7 <th_77@naver.com>
2024-06-13 20:55:42 +0900

    저장시 axios 컴포저블 함수 구현
    - method를 post로 전달
    - execute를 가져와서 저장시 excute
    - 성공이나 실패시 처리 로직 전달

--- vue3-perfect-final/vue3-posts/src/views/posts/PostCreateView.vue
+++ vue3-perfect-final/vue3-posts/src/views/posts/PostCreateView.vue
@@ -37,10 +37,10 @@
 <script setup>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
-import { createPost } from '@/api/posts';
 import PostForm from '@/components/posts/PostForm.vue';
 import { useAlert } from '@/composables/alert';
 import AppError from '@/components/app/AppError.vue';
+import { useAxios } from '@/hooks/useAxios';
 
 const { vAlert, vSuccess } = useAlert();
 
@@ -51,28 +51,47 @@ const form = ref({
 });
 
 // 데이터 등록 로딩, 에러
-const loading = ref(false);
-const error = ref(null);
-
+const { error, loading, execute } = useAxios(
+	'/posts',
+	{
+		mehtod: 'post',
+		data: { ...form.value, createdAt: Date.now() },
+	},
+	{
+		immediate: false,
+		onSuccess: () => {
+			router.push({ name: 'PostList' });
+			vSuccess('등록이 완료되었습니다.');
+		},
+		onError: err => {
+			vAlert(err.message);
+		},
+	},
+);
 // 저장
 const save = async () => {
-	try {
-		loading.value = true;
-		// 생성api
-		await createPost({
-			...form.value,
-			createdAt: Date.now(),
-		});
-		// 등록이 성공되면 리스트로 이동
-		router.push({ name: 'PostList' });
-		vSuccess('등록이 완료되었습니다.');
-	} catch (err) {
-		vAlert(err.message);
-		error.value = err;
-	} finally {
-		loading.value = false;
-	}
+	execute({ ...form.value, createdAt: Date.now() });
 };
+
+// const save = async () => {
+// 	try {
+// 		loading.value = true;
+// 		// 생성api
+// 		await createPost({
+// 			...form.value,
+// 			createdAt: Date.now(),
+// 		});
+// 		// 등록이 성공되면 리스트로 이동
+// 		router.push({ name: 'PostList' });
+// 		vSuccess('등록이 완료되었습니다.');
+// 	} catch (err) {
+// 		vAlert(err.message);
+// 		error.value = err;
+// 	} finally {
+// 		loading.value = false;
+// 	}
+// };
+
 const goListPage = () => router.push({ name: 'PostList' });
 </script>
 
